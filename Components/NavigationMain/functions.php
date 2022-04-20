5f738b381052b3e80c5bd01b7b820ecae00dc138
Laura Brown <laura+git@laurabrown.studio>
2022-04-20 12:09:58 +0200

    rename menus

--- Components/NavigationMain/functions.php
+++ Components/NavigationMain/functions.php
@@ -7,7 +7,7 @@ use Timber\Menu;
 
 add_action('init', function () {
     register_nav_menus([
-        'navigation_main' => __('Navigation Main', 'flynt')
+        'navigation_main' => __('Header Navigation', 'flynt')
     ]);
 });
 
